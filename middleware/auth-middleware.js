4510e67a317837a4171a1ffc5f21bea6fc36b193
Dave Vazquez <dmvaz.li@gmail.com>
2019-09-06 00:27:38 -0400

    implemented authentication middleware

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,5 +1,31 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  next();
+  const tokenHeader = req.headers.authorization;
+  if (tokenHeader) {
+    const [bearer, token] = tokenHeader.split(' ');
+
+    if (bearer.toUpperCase() === 'BEARER' && token) {
+      jwt.verify(
+        token,
+        process.env.LOGIN_SECRET || 'sooperdoopersecret',
+        (err, decodedToken) => {
+          if (err) {
+            res
+              .status(401)
+              .json({ message: 'error verifying token', error: err.message });
+          } else {
+            req.decodedJWT = decodedToken;
+            next();
+          }
+        }
+      );
+    } else {
+      res
+        .status(401)
+        .json({ message: 'invalid scheme, or no token after scheme name.' });
+    }
+  } else {
+    res.status(401).json({ message: 'missing authorization header' });
+  }
 };
