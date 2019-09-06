839d5470eaaecfdbbe3efa84b615269ac51bf889
Dave Vazquez <dmvaz.li@gmail.com>
2019-09-06 00:23:46 -0400

    implemented queries to users table

--- routes/auth/auth-model.js
+++ routes/auth/auth-model.js
@@ -1,3 +1,27 @@
 const db = require('../../data/db-config');
 
-module.exports = {};
+module.exports = {
+  add,
+  findBy,
+  findById
+};
+
+function add(user) {
+  return db('users')
+    .insert(user)
+    .then(async ([id]) => {
+      return await findById(id);
+    });
+}
+
+function findBy(filter) {
+  return db('users')
+    .where(filter)
+    .first();
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
