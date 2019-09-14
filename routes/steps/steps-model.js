dcc4faa345e4f32f6924aa2e77737fc17c646924
Dave Vazquez <dmvaz.li@gmail.com>
2019-09-14 18:34:56 -0400

    added returning clause to query to run w/postgres

--- routes/steps/steps-model.js
+++ routes/steps/steps-model.js
@@ -1,4 +1,4 @@
-const db = require("../../data/db-config");
+const db = require('../../data/db-config');
 
 module.exports = {
   find,
@@ -10,26 +10,26 @@ module.exports = {
 };
 
 function find() {
-  return db("ingredients");
+  return db('ingredients');
 }
 
 function findById(id) {
-  return db("steps")
+  return db('steps')
     .where({ id })
     .first();
 }
 
 function getSteps(recipe_id) {
   return db
-    .select("steps.recipe_id", "steps.description")
-    .from("steps")
-    .join("recipes", "recipes.id", "steps.recipe_id")
-    .where("steps.recipe_id", recipe_id);
+    .select('steps.recipe_id', 'steps.description')
+    .from('steps')
+    .join('recipes', 'recipes.id', 'steps.recipe_id')
+    .where('steps.recipe_id', recipe_id);
 }
 
 function addSteps(stepsData) {
   return (
-    db("steps")
+    db('steps')
       .insert(stepsData)
       .then(async _ => {
         return await findById(stepsData.id);
@@ -43,13 +43,13 @@ function addSteps(stepsData) {
 }
 
 async function update(changes, id) {
-  return db("steps")
+  return db('steps')
     .where({ id })
     .update(changes);
 }
 
 function remove(id) {
-  return db("steps")
+  return db('steps')
     .where({ id })
     .del();
 }
