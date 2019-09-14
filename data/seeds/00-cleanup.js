9b5bf620ecd0c04195483b0b7781c85ff9a51f95
Dave Vazquez <dmvaz.li@gmail.com>
2019-09-14 13:19:04 -0400

    postgres configurations

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,7 +1,12 @@
 exports.seed = async function(knex) {
   try {
+    await knex.truncate('ingredients');
     await knex.truncate('measurements');
+    await knex.truncate('steps');
+    await knex.truncate('recipes_categories');
+    await knex.truncate('recipes');
     await knex.truncate('categories');
+    await knex.truncate('users');
   } catch (err) {
     console.error(err);
   }
